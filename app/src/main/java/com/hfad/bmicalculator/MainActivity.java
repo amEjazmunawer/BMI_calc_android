b92897b3795668f3f9bd14e1950f4d902254cafe
amEjazmunawer <firelightwave89@gmail.com>
2019-01-17 18:24:54 +0800

    Add layout and same basic coding

--- app/src/main/java/com/hfad/bmicalculator/MainActivity.java
+++ app/src/main/java/com/hfad/bmicalculator/MainActivity.java
@@ -2,12 +2,36 @@ package com.hfad.bmicalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myButtonListenerMethod();
+
     }
+
+    public void myButtonListenerMethod() {
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText wt = (EditText) findViewById(R.id.editTextWeight);
+                EditText ht = (EditText) findViewById(R.id.editTextHeight);
+                Double weight = Double.parseDouble( wt.getText().toString());
+                Double height = Double.parseDouble( ht.getText().toString());
+
+
+
+
+
+            }});
+    }
+
 }
